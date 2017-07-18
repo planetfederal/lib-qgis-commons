562f0b531836c961064e8ed7105545f148d99f5c
volaya <volayaf@gmail.com>
2017-07-18 11:52:50 +0200

    changed version in setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="qgiscommons",
-    version="0.1.4",
+    version="0.1.5",
     author="Victor Olaya",
     author_email="volaya@boundlessgeo.com",
     description="Common functions and classes to be used in QGIS plugins",    
